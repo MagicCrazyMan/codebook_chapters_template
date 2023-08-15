6bb3c1ea3f9f8af3afa6277ab1316a3a9a31a507
MagicCrazyMan <350088648@qq.com>
2023-08-15 15:24:00 +0800

    wip

--- chapters/03_illumination/03_specular/index.js
+++ chapters/03_illumination/03_specular/index.js
@@ -1,258 +1,190 @@
-import { glMatrix, mat4, vec3 } from "gl-matrix";
-import { bindWebGLProgram, getCanvasResizeObserver, getWebGLContext } from "../../libs/common";
+import { glMatrix, quat, vec3 } from "gl-matrix";
+import { BufferAttribute, BufferDescriptor } from "../../libs/Attribute";
+import { CullFace, DrawMode, UniformType } from "../../libs/Constants";
+import { Scene } from "../../libs/Scene";
+import { Uniform } from "../../libs/Uniform";
+import { CameraUniformNames } from "../../libs/camera/Camera";
+import { PerspectiveCamera } from "../../libs/camera/Perspective";
+import { getCanvas, watchInput, watchInputs } from "../../libs/common";
+import { EntityAttributeNames, EntityUniformNames } from "../../libs/entity/RenderEntity";
+import { IndexedCube } from "../../libs/geom/Cube";
+import {
+  EntityAttributeBinding,
+  EntityUniformBinding,
+  MainCameraUniformBinding,
+  Material,
+  MaterialAttributeBinding,
+  MaterialUniformBinding,
+} from "../../libs/material/Material";
 
-const vertexShader = `
-  attribute vec4 a_Position;
-  attribute vec3 a_SpecularReflection;
-  attribute vec4 a_Normal;
-
-  uniform mat4 u_MvpMatrix;
-  uniform mat4 u_ModelMatrix;
-  uniform mat4 u_NormalMatrix;
-
-  varying vec3 v_SpecularReflection;
-  varying vec3 v_Normal;
-  varying vec3 v_Position;
-
-  void main() {
-    gl_Position = u_MvpMatrix * a_Position;
-    v_Position = vec3(u_ModelMatrix * a_Position);
-    v_Normal = vec3(u_NormalMatrix * a_Normal);
-    v_SpecularReflection = a_SpecularReflection;
+class SpecularLight extends Material {
+  name() {
+    return "DiffuseLight";
   }
-`;
-const fragmentShader = `
-  #ifdef GL_FRAGMENT_PRECISION_HIGH
-    precision highp float;
-  #else
-    precision mediump float;
-  #endif
 
-  uniform vec3 u_LightColor;
-  uniform vec3 u_LightPosition;
-  uniform float u_LightSpecularShininessExponent;
-
-  uniform vec3 u_CameraPosition;
-
-  varying vec3 v_SpecularReflection;
-  varying vec3 v_Normal;
-  varying vec3 v_Position;
-
-  void main() {
-    // normalizes normal vector
-    vec3 normal = normalize(v_Normal);
-    // calculates light direction
-    vec3 lightDirection = normalize(u_LightPosition - v_Position);
-    // calculates reflection direction
-    vec3 reflectionDirection = 2.0 * normal * dot(normal, lightDirection) - lightDirection;
-    reflectionDirection = normalize(reflectionDirection);
-
-    // calculates vector from object position to camera and normalize
-    vec3 cameraDirection = normalize(u_CameraPosition - v_Position);
-    // calculates cosine angle between r and v
-    float cosine = max(dot(reflectionDirection, cameraDirection), 0.0);
-    // calculates specular power
-    float power = pow(cosine, u_LightSpecularShininessExponent);
-
-    // calculates specular light color
-    vec3 color = v_SpecularReflection * u_LightColor * power;
-
-    gl_FragColor = vec4(color, 1.0);
+  vertexShaderSource() {
+    return `
+      attribute vec4 a_Position;
+      attribute vec4 a_Normal;
+    
+      uniform mat4 u_MvpMatrix;
+      uniform mat4 u_ModelMatrix;
+      uniform mat4 u_NormalMatrix;
+    
+      attribute vec3 a_SpecularReflection;
+      varying vec3 v_SpecularReflection;
+      varying vec3 v_Normal;
+      varying vec3 v_Position;
+    
+      void main() {
+        gl_Position = u_MvpMatrix * a_Position;
+        v_Position = vec3(u_ModelMatrix * a_Position);
+        v_Normal = vec3(u_NormalMatrix * a_Normal);
+        v_SpecularReflection = a_SpecularReflection;
+      }
+    `;
   }
-`;
 
-const gl = getWebGLContext();
-const program = bindWebGLProgram(gl, [
-  { source: vertexShader, type: gl.VERTEX_SHADER },
-  { source: fragmentShader, type: gl.FRAGMENT_SHADER },
-]);
+  fragmentShaderSource() {
+    return `
+      #ifdef GL_FRAGMENT_PRECISION_HIGH
+        precision highp float;
+      #else
+        precision mediump float;
+      #endif
+    
+      varying vec3 v_SpecularReflection;
 
-/**
- * Setups mvp, normal matrix and camera position
- */
-const uMvpMatrix = gl.getUniformLocation(program, "u_MvpMatrix");
-const uModelMatrix = gl.getUniformLocation(program, "u_ModelMatrix");
-const uNormalMatrix = gl.getUniformLocation(program, "u_NormalMatrix");
-const uCameraPosition = gl.getUniformLocation(program, "u_CameraPosition");
-const rps = glMatrix.toRadian(20); // Radian Per Second
-let lastAnimationTime = 0;
-let currentRotation = 0;
-const modelMatrix = mat4.create();
-const cameraPosition = vec3.fromValues(6, 6, 14);
-gl.uniform3fv(uCameraPosition, cameraPosition); // set camera position
-const viewMatrix = mat4.lookAt(
-  mat4.create(),
-  cameraPosition,
-  vec3.fromValues(0, 0, 0),
-  vec3.fromValues(0, 1, 0)
-);
-const projectionMatrix = mat4.create();
-const mvpMatrix = mat4.create();
-const normalMatrix = mat4.create();
-const setProjectionMatrix = () => {
-  mat4.perspective(
-    projectionMatrix,
+      uniform vec3 u_LightPosition;
+      uniform vec3 u_SpecularLightColor;
+      uniform float u_SpecularLightShininessExponent;
+    
+      uniform vec3 u_CameraPosition;
+    
+      varying vec3 v_Normal;
+      varying vec3 v_Position;
+    
+      void main() {
+        // normalizes normal vector
+        vec3 normal = normalize(v_Normal);
+        // calculates light direction
+        vec3 lightDirection = normalize(u_LightPosition - v_Position);
+        // calculates reflection direction
+        vec3 reflectionDirection = 2.0 * normal * dot(normal, lightDirection) - lightDirection;
+        reflectionDirection = normalize(reflectionDirection);
+    
+        // calculates vector from object position to camera and normalize
+        vec3 cameraDirection = normalize(u_CameraPosition - v_Position);
+        // calculates cosine angle between r and v
+        float cosine = max(dot(reflectionDirection, cameraDirection), 0.0);
+        // calculates specular power
+        float power = pow(cosine, u_SpecularLightShininessExponent);
+    
+        // calculates specular light color
+        vec3 color = v_SpecularReflection * u_SpecularLightColor * power;
+    
+        gl_FragColor = vec4(color, 1.0);
+      }
+    `;
+  }
+
+  attributesBindings() {
+    return [
+      new EntityAttributeBinding(EntityAttributeNames.Position),
+      new EntityAttributeBinding(EntityAttributeNames.Normal),
+      new MaterialAttributeBinding("a_SpecularReflection"),
+    ];
+  }
+
+  uniformBindings() {
+    return [
+      new EntityUniformBinding(EntityUniformNames.MvpMatrix),
+      new EntityUniformBinding(EntityUniformNames.ModelMatrix),
+      new EntityUniformBinding(EntityUniformNames.NormalMatrix),
+      new MaterialUniformBinding("u_LightPosition"),
+      new MaterialUniformBinding("u_SpecularLightColor"),
+      new MaterialUniformBinding("u_SpecularLightShininessExponent"),
+      new MainCameraUniformBinding(CameraUniformNames.Position),
+    ];
+  }
+
+  drawMode() {
+    return DrawMode.Triangles;
+  }
+
+  specularLightColor = vec3.create();
+  specularLightShininessExponent = new Float32Array(1);
+
+  lightPosition = vec3.fromValues(5, -2, 5);
+
+  // prettier-ignore
+  cubeFaceReflections = new Float32Array([
+    0.4,0.4,1.0,  0.4,0.4,1.0,  0.4,0.4,1.0,  0.4,0.4,1.0,  // front(blue)
+    1.0,0.4,0.4,  1.0,0.4,0.4,  1.0,0.4,0.4,  1.0,0.4,0.4,  // up(red)
+    0.4,1.0,1.0,  0.4,1.0,1.0,  0.4,1.0,1.0,  0.4,1.0,1.0,  // back(cyan)
+    1.0,1.0,1.0,  1.0,1.0,1.0,  1.0,1.0,1.0,  1.0,1.0,1.0,  // bottom(white)
+    1.0,1.0,0.4,  1.0,1.0,0.4,  1.0,1.0,0.4,  1.0,1.0,0.4,  // left(yellow)
+    0.4,1.0,0.4,  0.4,1.0,0.4,  0.4,1.0,0.4,  0.4,1.0,0.4,  // right(green)
+  ]);
+
+  constructor() {
+    super();
+    this.uniforms.set(
+      "u_SpecularLightColor",
+      new Uniform(UniformType.FloatVector3, this.specularLightColor)
+    );
+    this.uniforms.set(
+      "u_SpecularLightShininessExponent",
+      new Uniform(UniformType.FloatVector1, this.specularLightShininessExponent)
+    );
+    this.uniforms.set("u_LightPosition", new Uniform(UniformType.FloatVector3, this.lightPosition));
+
+    this.attributes.set(
+      "a_SpecularReflection",
+      new BufferAttribute(new BufferDescriptor(this.cubeFaceReflections), 3)
+    );
+  }
+}
+
+const canvas = getCanvas();
+const scene = new Scene(canvas, {
+  cullFace: CullFace.Back,
+  camera: new PerspectiveCamera(
     glMatrix.toRadian(30),
-    gl.canvas.width / gl.canvas.height,
+    canvas.width / canvas.height,
     1,
-    100
-  );
-};
-const setModelMatrix = (time) => {
-  currentRotation += ((time - lastAnimationTime) / 1000) * rps;
-  currentRotation %= 2 * Math.PI;
-  mat4.fromYRotation(modelMatrix, currentRotation);
-  gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix);
-};
-const setMvpMatrix = () => {
-  mat4.identity(mvpMatrix);
-  mat4.multiply(mvpMatrix, mvpMatrix, projectionMatrix);
-  mat4.multiply(mvpMatrix, mvpMatrix, viewMatrix);
-  mat4.multiply(mvpMatrix, mvpMatrix, modelMatrix);
-  gl.uniformMatrix4fv(uMvpMatrix, false, mvpMatrix);
-};
-const setNormalMatrix = () => {
-  mat4.invert(normalMatrix, modelMatrix);
-  mat4.transpose(normalMatrix, normalMatrix);
-  gl.uniformMatrix4fv(uNormalMatrix, false, normalMatrix);
-};
-getCanvasResizeObserver(() => {
-  setProjectionMatrix();
-  setMvpMatrix();
+    1000,
+    vec3.fromValues(6, 6, 14)
+  ),
 });
 
-/**
- * Setups cube normals
- */
-// prettier-ignore
-const normals = new Float32Array([
-  0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
-  1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
-  0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
- -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
-  0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
-  0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
-]);
-const normalsBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-const uNormals = gl.getAttribLocation(program, "a_Normal");
-gl.vertexAttribPointer(uNormals, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(uNormals);
+const specularLight = new SpecularLight();
 
-/**
- * Setups light color
- */
-const uLightColor = gl.getUniformLocation(program, "u_LightColor");
-const lightColorInputs = [
-  document.getElementById("colorR"),
-  document.getElementById("colorG"),
-  document.getElementById("colorB"),
-];
-lightColorInputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    setLightColor();
-  });
+const cube = new IndexedCube(4, 4, 4);
+cube.material = specularLight;
+scene.root.addChild(cube);
+
+const dps = 20; // Degrees Per Second
+const rotation = quat.create();
+scene.event.addEventListener("prerender", (event) => {
+  /**@type {import("../../libs/WebGLRenderer").FrameState} */
+  const frameState = event.frameState;
+  let r = (frameState.previousTime / 1000) * dps;
+  r %= 360;
+  cube.setRotationQuaternion(quat.fromEuler(rotation, 0, r, 0), true);
 });
-const setLightColor = () => {
-  gl.uniform3f(
-    uLightColor,
-    parseFloat(lightColorInputs[0].value),
-    parseFloat(lightColorInputs[1].value),
-    parseFloat(lightColorInputs[2].value)
-  );
-};
-setLightColor();
+
+scene.startRendering();
 
 /**
- * Setups light position
+ * Setups specular light color
  */
-const uLightPosition = gl.getUniformLocation(program, "u_LightPosition");
-gl.uniform3fv(uLightPosition, vec3.fromValues(5, -2, 5));
-
+watchInputs(["specularColorR", "specularColorG", "specularColorB"], ([r, g, b]) => {
+  vec3.set(specularLight.specularLightColor, parseFloat(r), parseFloat(g), parseFloat(b));
+});
 /**
  * Setups light specular shininess exponent
  */
-const uLightSpecularShininessExponent = gl.getUniformLocation(
-  program,
-  "u_LightSpecularShininessExponent"
-);
-const specularShininessExponentInput = document.getElementById("specularShininessExponent");
-specularShininessExponentInput.addEventListener("input", () => {
-  setLightSpecularShininessExponent();
+watchInput("specularShininessExponent", (value) => {
+  specularLight.specularLightShininessExponent[0] = parseFloat(value);
 });
-const setLightSpecularShininessExponent = () => {
-  gl.uniform1f(uLightSpecularShininessExponent, parseFloat(specularShininessExponentInput.value));
-};
-setLightSpecularShininessExponent();
-
-/**
- * Setups cube
- *   v6----- v5
- *  /|      /|
- * v1------v0|
- * | |     | |
- * | |v7---|-|v4
- * |/      |/
- * v2------v3
- */
-// prettier-ignore
-const vertices = new Float32Array([
-  2.0, 2.0, 2.0,  -2.0, 2.0, 2.0,  -2.0,-2.0, 2.0,   2.0,-2.0, 2.0,  // v0-v1-v2-v3 front
-  2.0, 2.0, 2.0,   2.0,-2.0, 2.0,   2.0,-2.0,-2.0,   2.0, 2.0,-2.0,  // v0-v3-v4-v5 right
-  2.0, 2.0, 2.0,   2.0, 2.0,-2.0,  -2.0, 2.0,-2.0,  -2.0, 2.0, 2.0,  // v0-v5-v6-v1 up
- -2.0, 2.0, 2.0,  -2.0, 2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0,-2.0, 2.0,  // v1-v6-v7-v2 left
- -2.0,-2.0,-2.0,   2.0,-2.0,-2.0,   2.0,-2.0, 2.0,  -2.0,-2.0, 2.0,  // v7-v4-v3-v2 down
-  2.0,-2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0, 2.0,-2.0,   2.0, 2.0,-2.0   // v4-v7-v6-v5 back
-]);
-const verticesBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-const aPosition = gl.getAttribLocation(program, "a_Position");
-gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(aPosition);
-
-// prettier-ignore
-const specularReflections = new Float32Array([
-  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
-  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
-  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
-  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left(yellow)
-  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 bottom(white)
-  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back(cyan)
-]);
-const specularReflectionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, specularReflectionBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, specularReflections, gl.STATIC_DRAW);
-const aSpecularReflection = gl.getAttribLocation(program, "a_SpecularReflection");
-gl.vertexAttribPointer(aSpecularReflection, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(aSpecularReflection);
-
-// prettier-ignore
-const indices = new Uint8Array([
-  0,  1,  2,  0,  2,  3,    // front
-  4,  5,  6,  4,  6,  7,    // right
-  8,  9, 10,  8, 10, 11,    // up
- 12, 13, 14, 12, 14, 15,    // left
- 16, 17, 18, 16, 18, 19,    // down
- 20, 21, 22, 20, 22, 23     // back
-]);
-const indicesBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-gl.enable(gl.DEPTH_TEST);
-gl.enable(gl.CULL_FACE);
-gl.cullFace(gl.BACK);
-const render = (time) => {
-  setModelMatrix(time);
-  setNormalMatrix();
-  setMvpMatrix();
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
-
-  requestAnimationFrame(render);
-  lastAnimationTime = time;
-};
-render(0);
