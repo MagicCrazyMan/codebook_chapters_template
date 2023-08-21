c7efc0adff1c3fdc6128ad9c47189da8b011f0f6
MagicCrazyMan <350088648@qq.com>
2023-08-21 20:31:36 +0800

    wip

--- chapters/libs/control/BlenderCamera.js
+++ chapters/libs/control/BlenderCamera.js
@@ -4,9 +4,10 @@ import { Control } from "./Control.js";
 /**
  * @typedef {Object} Options Keyboard camera options
  * @property {number} [rotation] Rotation offset. in radians. Default 3 degrees
- * @property {number} [movement] Movement offset each keyboard event. Default 0.2
+ * @property {number} [keyboardMovement] Movement offset each keyboard event. Default 0.2
  * @property {number} [wheelMovement] Movement offset each mouse wheel event. Default is `5 * movement`.
- * @property {number} [mouseMovement] Movement offset in pixel. Default equals 0.02 per pixel.
+ * @property {number} [mouseMovement] Movement offset per pixel when mouse move. Default 0.02 per pixel.
+ * @property {number} [touchMovement] Movement offset per pixel when touch move. Default 0.02 per pixel.
  * @property {number} [radius] Initial camera radius. Available only when `direction` is true. Default 5.
  * @property {number} [forwardLimitZoneRadius] Limit sphere zone limitation radius. Default 0.25.
  * @property {import("gl-matrix").ReadonlyVec3} [lookAt] Camera look at position. Default `(0, 0, 0)`.
@@ -55,11 +56,18 @@ export class BlenderCamera extends Control {
   wheelMovement;
 
   /**
-   * Movement offset in pixel.
+   * Movement offset in pixel when mouse move.
    * @type {number}
    * @readonly
    */
   mouseMovement;
+
+  /**
+   * Movement offset in pixel when touch move.
+   * @type {number}
+   * @readonly
+   */
+  touchMovement;
 
   /**
    * @type {import("../Scene.js").Scene | undefined}
@@ -138,15 +146,31 @@ export class BlenderCamera extends Control {
   _mouseMoveHandler;
 
   /**
+   * @private
+   */
+  _touchStartHandler;
+
+  /**
+   * @private
+   */
+  _touchMoveHandler;
+
+  /**
+   * @private
+   */
+  _touchEndHandler;
+
+  /**
    * Constructs a new camera keyboard controller
    * @param {Options} [opts] Keyboard camera options
    */
   constructor(opts = {}) {
     super();
     this.rotation = opts.rotation ?? glMatrix.toRadian(3);
-    this.keyboardMovement = opts.movement ?? 0.2;
+    this.keyboardMovement = opts.keyboardMovement ?? 0.2;
     this.wheelMovement = opts.wheelMovement ?? this.keyboardMovement * 5;
     this.mouseMovement = opts.mouseMovement ?? 0.02;
+    this.touchMovement = opts.touchMovement ?? 0.02;
     this.forwardLimitZoneRadius = opts.forwardLimitZoneRadius ?? 0.25;
 
     const upVector = opts.up ?? vec3.set(this._tmpVec3_2, 0, 1, 0);
@@ -475,7 +499,7 @@ export class BlenderCamera extends Control {
     const offsetY = e.y - this._previousMousePosition[1];
 
     if (e.shiftKey) {
-      this.right(-offsetX * this.mouseMovement);
+      this.left(offsetX * this.mouseMovement);
       this.up(offsetY * this.mouseMovement);
     } else {
       const ratioX = offsetX / e.target.clientWidth;
@@ -489,6 +513,196 @@ export class BlenderCamera extends Control {
 
     this._previousMousePosition[0] = e.x;
     this._previousMousePosition[1] = e.y;
+  }
+
+  /**
+   * @type {number[] | undefined}
+   * @private
+   */
+  _previousTouchPosition;
+
+  /**
+   * @type {number[][] | undefined}
+   * @private
+   */
+  _previousTowTouchesPositions;
+
+  /**
+   * @type {number | undefined}
+   * @private
+   */
+  _previousTowTouchesCalibre;
+
+  /**
+   * @type {number | undefined}
+   * @private
+   */
+  _touchHoldingTimer;
+
+  /**
+   * @type {number}
+   * @private
+   */
+  _touchHoldingTimeout = 200;
+
+  /**
+   * @type {number}
+   * @private
+   */
+  _touchHoldingLimitRadius = 5;
+
+  /**
+   * @type {number | undefined}
+   * @private
+   */
+  _touchHoldingIdentifier;
+
+  /**
+   * Start touch holding detecting procedure.
+   * 
+   * If procedure finished, two touches falls into ROTATING mode.
+   * Otherwise, two touches falls into ZOOMING mode.
+   * @private
+   * @param {number} identifier
+   */
+  startTouchHoldTimer(identifier) {
+    this._touchHoldingTimer = setTimeout(() => {
+      this._touchHoldingIdentifier = identifier;
+      this._touchHoldingTimer = undefined;
+    }, this._touchHoldingTimeout);
+  }
+
+  /**
+   * @private
+   */
+  stopTouchHoldTimer() {
+    if (this._touchHoldingTimer) {
+      clearTimeout(this._touchHoldingTimer);
+      this._touchHoldingTimer = undefined;
+    }
+  }
+
+  /**
+   * @private
+   * @param {TouchEvent} e
+   */
+  onTouch(e) {
+    if (e.touches.length === 1) {
+      // start touch holding detecting procedure
+      this.startTouchHoldTimer(e.touches[0].identifier);
+
+      this._previousTouchPosition = [
+        e.touches[0].identifier,
+        e.touches[0].clientX,
+        e.touches[0].clientY,
+      ];
+    } else if (e.touches.length === 2) {
+      this.stopTouchHoldTimer();
+
+      this._previousTowTouchesPositions = [
+        [e.touches[0].identifier, e.touches[0].clientX, e.touches[0].clientY],
+        [e.touches[1].identifier, e.touches[1].clientX, e.touches[1].clientY],
+      ];
+
+      const dx = e.touches[1].clientX - e.touches[0].clientX;
+      const dy = e.touches[1].clientY - e.touches[0].clientY;
+      this._previousTowTouchesCalibre = Math.hypot(dx, dy);
+      this._previousTowTouchesAngle = Math.atan2(dy, dx);
+    } else {
+      this.stopTouchHoldTimer();
+
+      this._previousTouchPosition = undefined;
+      this._previousTowTouchesPositions = undefined;
+      this._previousTowTouchesCalibre = undefined;
+      this._touchHoldingIdentifier = undefined;
+      return;
+    }
+
+    e.preventDefault();
+  }
+
+  /**
+   * @private
+   * @param {TouchEvent} e
+   */
+  onTouchMove(e) {
+    if (e.touches.length === 1) {
+      // single touch
+      const touch = e.touches[0];
+
+      const offsetX = touch.clientX - this._previousTouchPosition[1];
+      const offsetY = touch.clientY - this._previousTouchPosition[2];
+
+      /**
+       * If holding detecting procedure running,
+       * check whether movement radius exceed the holding limit radius.
+       * If true, stop holding detecting procedure and set two touches mode to ZOOMING mode,
+       * and do panning.
+       * If false, stop panning and continue holding detecting procedure.
+       */
+      if (this._touchHoldingTimer) {
+        const radius = Math.hypot(offsetX, offsetY);
+        if (radius > this._touchHoldingLimitRadius) {
+          this.stopTouchHoldTimer();
+        } else {
+          return;
+        }
+      }
+
+      // PANNING
+      this.left(offsetX * this.touchMovement);
+      this.up(offsetY * this.touchMovement);
+
+      this._previousTouchPosition[0] = touch.identifier;
+      this._previousTouchPosition[1] = touch.clientX;
+      this._previousTouchPosition[2] = touch.clientY;
+    } else if (e.touches.length === 2) {
+      //  touches
+      if (this._touchHoldingIdentifier === undefined) {
+        // ZOOMING mode
+        const dx = e.touches[1].clientX - e.touches[0].clientX;
+        const dy = e.touches[1].clientY - e.touches[0].clientY;
+
+        const calibre = Math.hypot(dx, dy);
+        const calibreOffset = calibre - this._previousTowTouchesCalibre;
+        if (calibreOffset > 0) {
+          this.forward(4 * this.touchMovement);
+        } else {
+          this.backward(4 * this.touchMovement);
+        }
+        this._previousTowTouchesCalibre = calibre;
+      } else {
+        // ROTATING mode
+        const previousChangedTouch =
+          this._previousTowTouchesPositions[0][0] === this._touchHoldingIdentifier
+            ? this._previousTowTouchesPositions[1]
+            : this._previousTowTouchesPositions[0];
+        const currentChangedTouch =
+          e.touches[0].identifier === this._touchHoldingIdentifier ? e.touches[1] : e.touches[0];
+
+        const offsetX = currentChangedTouch.clientX - previousChangedTouch[1];
+        const offsetY = currentChangedTouch.clientY - previousChangedTouch[2];
+
+        const ratioX = offsetX / e.target.clientWidth;
+        const horizontalAngle = ratioX * 2 * Math.PI;
+        this.horizontalRotate(-horizontalAngle);
+
+        const ratioY = offsetY / e.target.clientHeight;
+        const verticalAngle = ratioY * 2 * Math.PI;
+        this.verticalRotate(-verticalAngle);
+
+        this._previousTowTouchesPositions[0][0] = e.touches[0].identifier;
+        this._previousTowTouchesPositions[0][1] = e.touches[0].clientX;
+        this._previousTowTouchesPositions[0][2] = e.touches[0].clientY;
+        this._previousTowTouchesPositions[1][0] = e.touches[1].identifier;
+        this._previousTowTouchesPositions[1][1] = e.touches[1].clientX;
+        this._previousTowTouchesPositions[1][2] = e.touches[1].clientY;
+      }
+    } else {
+      return;
+    }
+
+    e.preventDefault();
   }
 
   /**
@@ -515,19 +729,30 @@ export class BlenderCamera extends Control {
     this._mouseMoveHandler = (e) => {
       this.onMouseMove(e);
     };
+    this._touchStartHandler = (e) => {
+      this.onTouch(e);
+    };
+    this._touchEndHandler = (e) => {
+      this.onTouch(e);
+    };
+    this._touchMoveHandler = (e) => {
+      this.onTouchMove(e);
+    };
     window.addEventListener("keydown", this._keyboardHandler);
     scene.canvas.addEventListener("wheel", this._wheelHandler, { passive: false });
     scene.canvas.addEventListener("mousedown", this._mouseDownHandler);
     scene.canvas.addEventListener("mouseup", this._mouseUpHandler);
     scene.canvas.addEventListener("mousemove", this._mouseMoveHandler);
+    scene.canvas.addEventListener("touchstart", this._touchStartHandler);
+    scene.canvas.addEventListener("touchend", this._touchEndHandler);
+    scene.canvas.addEventListener("touchmove", this._touchMoveHandler);
   }
 
   /**
    * Unregister control from scene
    */
   unregister() {
-    if (this._keyboardHandler)
-    window.removeEventListener("keydown", this._keyboardHandler);
+    if (this._keyboardHandler) window.removeEventListener("keydown", this._keyboardHandler);
     if (this._wheelHandler)
       this.scene?.canvas.removeEventListener("wheel", this._wheelHandler, { passive: false });
     if (this._mouseDownHandler)
@@ -536,6 +761,12 @@ export class BlenderCamera extends Control {
       this.scene?.canvas.removeEventListener("mouseup", this._mouseUpHandler);
     if (this._mouseMoveHandler)
       this.scene?.canvas.removeEventListener("mousemove", this._mouseMoveHandler);
+    if (this._touchStartHandler)
+      this.scene?.canvas.removeEventListener("touchstart", this._touchStartHandler);
+    if (this._touchEndHandler)
+      this.scene?.canvas.removeEventListener("touchend", this._touchEndHandler);
+    if (this._touchMoveHandler)
+      this.scene?.canvas.removeEventListener("touchmove", this._touchMoveHandler);
 
     this.scene = undefined;
   }
